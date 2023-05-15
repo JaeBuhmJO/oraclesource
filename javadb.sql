49c1de7b4ca827c295f3ba116b8388f1c6f84b68
Jaebuhm <favor99531@gmail.com>
2023-05-15 12:15:48 +0900

    tables for spring

--- javadb.sql
+++ javadb.sql
@@ -13,8 +13,6 @@ MOBILE VARCHAR2(13)
 
 --select(+서브쿼리, 조인) + DML(insert, delete, update)
 
-select * from usertbl;
-
 delete usertbl where addr='옆집';
 commit;
 
@@ -40,7 +38,7 @@ CREATE SEQUENCE USER_SEQ;
 
 INSERT INTO USERTBL VALUES(USER_SEQ.NEXTVAL, '홍길동2', '1992', '활빈당' , '010-1231-2312');
 
-select * from usertbl;
+update booktbl set price=16000 where code=1010;
 
 commit;
 
@@ -139,5 +137,166 @@ INSERT INTO BOOKTBL(CODE, TITLE, WRITER, PRICE)VALUES (1004,'자바 1000제','�
 INSERT INTO BOOKTBL(CODE, TITLE, WRITER, PRICE)VALUES (1005,'자바 프로그래밍 입문','박은종',30000);
 COMMIT;
 
+select * from booktbl;
 desc booktbl;
 alter table booktbl add description nvarchar2(100);
+
+-- member 테이블
+-- userid (영어, 숫자, 특수문자) 최대 12자까지 허용
+-- password (영어, 숫자, 특수문자) 최대 15자까지 허용
+-- name (한글)
+-- gender (한글: 남 or 여)
+-- email(적절하게)
+
+CREATE TABLE MEMBERTBL (
+USERID VARCHAR2(15) PRIMARY KEY,
+PASSWORD VARCHAR2(20) NOT NULL,
+NAME NVARCHAR2(10) NOT NULL,
+GENDER NVARCHAR2(2) NOT NULL,
+EMAIL VARCHAR2(50) NOT NULL
+);
+INSERT INTO MEMBERTBL VALUES('hong123','hong123@','홍길동','남','hong123@gmail.com');
+commit;
+select * from membertbl;
+
+-- 게시판 BOARD
+-- 글번호(bno 숫자, 시퀀스 삽입, PK 이름은 PK_BOARD), 작성자(name 한글), 비밀번호(password 숫자, 영문자), 제목(TITLE 한글), 
+-- 내용(CONTENT 한글), 파일첨부(ATTACH 파일명), 
+-- 답변글 작성시 참조되는 글번호(RE_REF 숫자), 답변글 레벨(RE_LEV 숫자), 답변글 순서(RE_SEQ 숫자)
+-- 조회수(CNT 숫자, DEFAULT 0), 작성날짜(REGDATE, DEFAULT로 SYSDATE)
+
+-- 테이블 생성
+CREATE TABLE BOARD (
+BNO NUMBER(8) CONSTRAINT PK_BOARD PRIMARY KEY ,
+NAME NVARCHAR2(10) NOT NULL,
+PASSWORD VARCHAR2(20) NOT NULL,
+TITLE NVARCHAR2(50) NOT NULL,
+CONTENT NVARCHAR2(1000) NOT NULL,
+ATTACH NVARCHAR2(100),
+RE_REF NUMBER(8) NOT NULL,
+RE_LEV NUMBER(8) NOT NULL,
+RE_SEQ NUMBER(8) NOT NULL,
+CNT NUMBER(8) DEFAULT 0,
+REGDATE DATE DEFAULT SYSDATE
+);
+
+-- 시퀀스 생성 BOARD_SEQ
+CREATE SEQUENCE BOARD_SEQ;
+
+select * from board;
+
+insert into board(bno, name, password, title, content, re_ref, re_lev, re_seq)
+(select board_seq.nextval,name,password,title,content,board_seq.currval,re_lev,re_seq from board);
+
+commit;
+
+-- 댓글
+--re_ref, re_lev, re_seq
+
+-- 원본글 작성 re_ref : bno  값과 동일
+--              re_lev : 0, re_seq : 0
+
+select bno, title, re_ref, re_lev, re_seq from board where bno=32;
+
+-- re_ref : 그룹번호, re_seq : 그룹 내에서 댓글의 순서, 
+-- re_lev : 그룹 내에서 댓글의 깊이(원본글의 댓글인지? 댓글의 댓글인지?)
+-- 댓글도 새 글임 => insert 작업
+--      bno : board_seq.nextval
+--      re_ref : 원본글의 re_ref와 동일
+--      re_seq : 원본글의 re_seq + 1
+--      re_lev : 원본글의 re_lev +1
+
+-- 첫번째 댓글 작성
+insert into board(bno,name,password,title,content,attach,re_ref,re_lev,re_seq)
+values(board_seq.nextval,'김댓글','12345','Re : 게시글','게시글 댓글',null,32,1,1);
+
+-- 두번째 댓글 작성
+-- re_seq가 값이 작을수록 최신글임 -> 기존 댓글의 re_seq를 수정해야함
+
+-- 기존 댓글이 있는가? 기존 댓글의 re_seq 변경을 한 후 insert 작업 해야함
+-- update 구문에서 where 절은 re_ref는 원본글의 re_ref값, re_seq 비교구문은 원본글의 re_seq 값과 비교
+update board set re_seq = re_seq + 1 where re_ref=32 and re_seq > 0 ;
+
+insert into board(bno,name,password,title,content,attach,re_ref,re_lev,re_seq)
+values(board_seq.nextval,'김댓글','12345','Re : 게시글2','게시글 댓글2',null,32,1,1);
+
+select bno, title, re_ref, re_lev, re_seq from board where re_ref=32 order by re_ref desc , re_seq;
+commit;
+
+-- 댓글의 댓글 작성
+-- 댓글은 무조건 update -> insert
+update board set re_seq = re_seq + 1 where re_ref=32 and re_seq > 2;
+
+insert into board(bno,name,password,title,content,attach,re_ref,re_lev,re_seq)
+values(board_seq.nextval,'김댓글','12345','ReRe : 게시글','댓글의 댓글2',null,32,2,3);
+
+-- 페이지 나누기
+-- ROWNUM : 조회된 결과에 번호를 매겨줌
+--          ORDER BY 구문에 INDEX가 들어가지 않는다면 제대로 된 결과를 보장하지 않음
+--          PK가 INDEX로 사용됨
+    SELECT ROWNUM, BNO, TITLE FROM BOARD ORDER BY BNO DESC;
+    
+    SELECT ROWNUM, BNO, TITLE, RE_REF, RE_LEV, RE_SEQ FROM BOARD ORDER BY RE_REF DESC, RE_SEQ;
+    
+-- 해결
+-- ORDER BY 구문을 먼저 실행한 후 ROWNUM 붙여야 함
+SELECT ROWNUM, BNO, TITLE, RE_REF, RE_LEV, RE_SEQ
+  FROM(SELECT ROWNUM, BNO, TITLE, RE_REF, RE_LEV, RE_SEQ FROM BOARD ORDER BY RE_REF DESC, RE_SEQ)
+  WHERE ROWNUM <= 30;
+  -- WHERE ROWNUM BETWEEN 2 AND 10; 이거 안됨. 1부터 해도 안됨.
+  
+  -- 1 2 3 4 5 6 ...
+  -- 1PAGE 요청 (1~30)
+  -- 2PAGE 요청 (31~60)
+  -- 3PAGE 요청 (61~90)
+  
+  --60개를 먼저 뽑아나와서, 31~60을 보여준다.
+  SELECT * FROM (SELECT ROWNUM AS RNUM, BNO, TITLE, RE_REF, RE_LEV, RE_SEQ
+  FROM(SELECT ROWNUM, BNO, TITLE, RE_REF, RE_LEV, RE_SEQ 
+  FROM BOARD ORDER BY RE_REF DESC, RE_SEQ) WHERE ROWNUM <= 60)
+  WHERE RNUM >30;
+  
+    --90개를 먼저 뽑아나와서, 61~90을 보여준다.
+  SELECT * FROM (SELECT ROWNUM AS RNUM, BNO, TITLE, RE_REF, RE_LEV, RE_SEQ
+  FROM(SELECT ROWNUM, BNO, TITLE, RE_REF, RE_LEV, RE_SEQ 
+  FROM BOARD ORDER BY RE_REF DESC, RE_SEQ) WHERE ROWNUM <= 90)
+  WHERE RNUM >60;
+  
+  -- spring_board
+  CREATE TABLE SPRING_BOARD(
+  BNO NUMBER(10) CONSTRAINT PK_SPRING_BOARD PRIMARY KEY,
+  TITLE VARCHAR2(200) NOT NULL,
+  CONTENT VARCHAR2(2000) NOT NULL,
+  WRITER VARCHAR2(50) NOT NULL,
+  REGDATE DATE DEFAULT SYSDATE,
+  UPDATEDATE DATE DEFAULT SYSDATE
+  );
+  
+  CREATE SEQUENCE SEQ_BOARD;
+  
+  select * from spring_board;
+  
+  -- 시퀀스 seq_board
+  create sequence seq_board;
+  
+  --mybatis 연습용 테이블
+  create table person(
+  id varchar2(20) primary key,
+  name varchar2(30) not null);
+  
+  insert into person values('choi123','최길동');
+  commit;
+  select * from person;
+  select * from booktbl order by code;
+  
+  -- 트랜잭션 테스트 테이블
+  -- 트랜잭션 : 하나의 업무에 여러개의 작은 업무들이 같이 묶여 있음 / 하나의 단위로 처리
+  -- EX) 계좌이체 : 해당 계좌 출금 + 타 계좌 입금
+  CREATE TABLE TBL_SAMPLE1(COL1 VARCHAR2(500));
+  CREATE TABLE TBL_SAMPLE2(COL1 VARCHAR2(50));
+  
+  select * from tbl_sample1;
+  select * from tbl_sample2;
+  
+  delete tbl_sample1;
+  commit;
